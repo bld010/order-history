ac29b2163a894c7855a8567b2c78f0ae6932d372
Brianna DelValle <briannadelvalle@gmail.com>
2019-08-29 10:15:42 -0600

    Write wrapper test and addNewPurchase method test to App.test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,8 +2,37 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
+
 describe('App', () => {
+
+  let wrapper = null;
+  let mockPurchases = [
+    {name: 'X'},
+    {name: 'Y'},
+    {name: 'Z'}
+  ]
+
+  beforeEach(()=> {
+    wrapper=shallow(<App />)
+  })
+
   it('should match the wrapper', () => {
-    
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should add new purchases', () => {
+    const mockEvent = {
+      preventDefault: () => {}
+    }
+    const mockNewPurchase = {name:'W'}
+    const mockPostNewPurchase = jest.fn();
+    wrapper.setState({purchases: mockPurchases});
+
+    wrapper.instance().addNewPurchase(mockEvent, mockNewPurchase)
+    wrapper.instance().postNewPurchase = mockPostNewPurchase;
+
+    const expected = wrapper.state().purchases
+
+    expect(expected).toEqual([...mockPurchases, mockNewPurchase])
   })
 })
